9604b20be76a251b32497c2792fddee5aca8bd2a
xemlock <xemlock@gmail.com>
2015-10-19 08:45:50 +0200

    removed setOptions()

--- src/HumusMvc/Dispatcher.php
+++ src/HumusMvc/Dispatcher.php
@@ -59,23 +59,6 @@ class Dispatcher extends StandardDispatcher implements
     }
 
     /**
-     * Set options from array
-     *
-     * @param array $options
-     * @return Dispatcher
-     */
-    public function setOptions(array $options)
-    {
-        foreach ($options as $key => $value) {
-            $method = 'set' . $key;
-            if (method_exists($this, $method)) {
-                $this->{$method}($value);
-            }
-        }
-        return $this;
-    }
-
-    /**
      * Returns TRUE if the Zend_Controller_Request_Abstract object can be
      * dispatched to a controller.
      *
